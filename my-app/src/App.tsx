8642d349c5ca01452a97c14d8bc4467becda0236
Kenny Nguyen <ken_nggg@outlook.com>
2020-07-17 15:38:57 +1200

    update website

--- my-app/src/App.tsx
+++ my-app/src/App.tsx
@@ -1,22 +1,20 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import logo from "./logo_2.svg";
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
+        <p>MSA 2020 Devops!!!</p>
         <a
           className="App-link"
-          href="https://reactjs.org"
+          href="https://msa.azurewebsites.net/"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          MSA Website
         </a>
       </header>
     </div>
